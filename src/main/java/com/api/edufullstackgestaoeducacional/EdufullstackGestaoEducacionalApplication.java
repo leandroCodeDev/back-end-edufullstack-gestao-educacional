0f0cb4bbe81faced351256c00f41cfe6a9ae0752
Leandro Dias <lvd@certi.org.br>
2024-04-09 00:59:22 -0300

    feat: projeto base do spring

---
+++ src/main/java/com/api/edufullstackgestaoeducacional/EdufullstackGestaoEducacionalApplication.java
@@ -0,0 +1,13 @@
+package com.api.edufullstackgestaoeducacional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdufullstackGestaoEducacionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdufullstackGestaoEducacionalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/edufullstackgestaoeducacional/EdufullstackGestaoEducacionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.edufullstackgestaoeducacional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdufullstackGestaoEducacionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
