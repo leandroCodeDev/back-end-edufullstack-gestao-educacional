e5a75b23c8c21c9e0103b9cb26fe27acba51627c
Leandro Dias <lvd@certi.org.br>
2024-04-11 23:43:34 -0300

    feat(cadastrar): adicionado retrição de acesso a rota cadastrar

--- src/main/java/com/api/edufullstackgestaoeducacional/services/TokenService.java
+++ src/main/java/com/api/edufullstackgestaoeducacional/services/TokenService.java
@@ -7,4 +7,15 @@ public interface TokenService {
     String gerarToken(UsuarioEntity user);
 
     String buscaCampo(String token, String claim);
+
+    void validateAdmin(String token);
+
+    void validatePedadogico(String token);
+
+    void validateRecruiter(String token);
+
+    void validateProfessor(String token);
+
+    void validateAluno(String token);
 }
+
