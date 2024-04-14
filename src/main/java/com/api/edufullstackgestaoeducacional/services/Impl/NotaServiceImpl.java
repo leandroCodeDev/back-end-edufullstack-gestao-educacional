787c2837c13755704dd1899b0be3a61db4e2643b
Leandro Dias <lvd@certi.org.br>
2024-04-14 03:52:31 -0300

    feat(nota): criado medoto atualiza nota

--- src/main/java/com/api/edufullstackgestaoeducacional/services/Impl/NotaServiceImpl.java
+++ src/main/java/com/api/edufullstackgestaoeducacional/services/Impl/NotaServiceImpl.java
@@ -71,7 +71,38 @@ public class NotaServiceImpl implements NotaService {
 
     @Override
     public ResponseNota atualizaNota(long id, RequestNota dto) {
-        return null;
+        NotaEntity nota = pegaNotaEntity(id).orElseThrow(() -> new NotFoundException("Nota não encontrado"));
+        DocenteEntity docente = docenteService.pegaDocenteEntity(dto.docenteId()).orElseThrow(() -> new NotFoundException("Docente não encontrado"));
+        if (!docente.getUsuario().getPerfil().getNome().equals("PROFESSOR")) {
+            throw new NotFoundException("O Docente não tem papel de Professor");
+        }
+        MateriaEntity materia = materiaService.pegaMateriaEntity(dto.materiaId()).orElseThrow(() -> new NotFoundException("Materia não encontrado"));
+
+        AlunoEntity aluno = alunoService.pegaAlunoEntity(dto.alunoId()).orElseThrow(() -> new NotFoundException("Aluno não encontrado"));
+
+        if (aluno.getTurma().getCurso().getId() != materia.getCurso().getId()) {
+            throw new NotFoundException("Aluno não matriculado ao curso dessa materia");
+        }
+
+
+        if (nota.getValor() != dto.valor()) {
+            nota.setValor(dto.valor());
+        }
+
+        if (materia.getId() != nota.getMateria().getId()) {
+            nota.setMateria(materia);
+        }
+
+        if (aluno.getId() != nota.getAluno().getId()) {
+            nota.setAluno(aluno);
+        }
+        if (docente.getId() != nota.getDocente().getId()) {
+            nota.setDocente(docente);
+        }
+        nota = repository.save(nota);
+        nota = pegaNotaEntity(nota.getId()).orElseThrow(() -> new NotFoundException("Nota não encontrado"));
+
+        return nota.toResponseNota();
     }
 
     @Override
